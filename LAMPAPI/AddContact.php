b5c3e34ee77be104b832fd1034af4bf99b97626e
Keating Sane <keatingsane@outlook.com>
2024-05-18 18:39:20 -0400

    Update AddContact.php

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,21 +1,21 @@
 <?php
 	$inData = getRequestInfo();
 	
-	$firstname = $inData["firstname"];
-	$lastname = $inData["lastname"];
-	$phone = $inData["phone"];
-	$email = $inData["email"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$phoneNumber = $inData["phoneNumber"];
+	$emailAddress = $inData["emailAddress"];
 	$userId = $inData["userId"];
 
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
+	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
-	} 
+	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (UserId, FirstName, LastName, Phone, Email) VALUES(?,?,?,?,?)");
-		$stmt->bind_param("sssss", $userId, $firstname, $lastname, $phone, $email);
+		$stmt = $conn->prepare("INSERT into Contacts (FirstName,LastName,PhoneNumber,EmailAddress, UserID) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("ssssi", $firstName, $lastName, $phoneNumber, $emailAddress, $userId);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
@@ -26,7 +26,7 @@
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
-
+	
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
@@ -38,5 +38,5 @@
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 ?>
