b1eb327cdb5f33049e5aece1968f07f1b7a34175
Keating Sane <keatingsane@outlook.com>
2024-05-18 18:48:31 -0400

    Update Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -6,7 +6,7 @@
 	$id = 0;
 	$firstName = "";
 	$lastName = "";
-
+	
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
 	if( $conn->connect_error )
 	{
@@ -14,30 +14,29 @@
 	}
 	else
 	{
-		// check if this username already exists in database
-		$stmt = $conn->prepare("SELECT ID from Users where Login=?");
-		$stmt->bind_param("s", $username);
+		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE (Login=? AND Password=?)");
+		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
-		if( $sresult->num_rows > 0 )
+		
+		if( $row = $result->fetch_assoc() )
 		{
-			returnWithError("Username already exists");
+			returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
 		}
 		else
 		{
-			
-			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+			returnWithError("No Records Found");
 		}
-
+		
 		$stmt->close();
 		$conn->close();
 	}
-	
+
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
-
+	
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
@@ -49,11 +48,11 @@
 		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 	function returnWithInfo( $firstName, $lastName, $id )
 	{
 		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 ?>
