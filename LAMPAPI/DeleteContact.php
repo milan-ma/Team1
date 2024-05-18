cb26c07688ebe2559126256f248b11ab18bfd90c
Keating Sane <keatingsane@outlook.com>
2024-05-18 18:43:58 -0400

    Update DeleteContact.php

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,18 +1,19 @@
 <?php
 	$inData = getRequestInfo();
 	
-	$contact = $inData["contact"];
 	$userId = $inData["userId"];
-
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
+	if($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
-	} 
+	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (UserId,Name) VALUES(?,?)");
-		$stmt->bind_param("ss", $userId, $contact);
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName = ? AND LastName = ? AND UserID = ?");
+		$stmt->bind_param("ssi", $firstName, $lastName, $userId);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
@@ -29,11 +30,11 @@
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 ?>
